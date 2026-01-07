795dd1edd0dcab76c2bf47e4f5124d26df3dd62a
soloopandit <saurabh7610091962@gmail.com>
2026-01-07 23:02:25 +0530

    Add files via upload

---
+++ ecomm.py
@@ -0,0 +1,43 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns 
+import streamlit as st
+
+
+def main():
+    st.title("this is my ecom app ")
+    st.sidebar.title("upload your file")
+
+    uploaded_file = st.sidebar.file_uploader("upload your own file", type= ['csv', 'xlsx'])
+
+    if uploaded_file is not None:
+        try :
+            if uploaded_file.name.endswith('.csv'):
+                data = pd.read_csv(uploaded_file)
+            else :
+                data = pd.read_excel(uploaded_file)
+
+            st.sidebar.success("file uploaded succesfully")
+
+            st.subheader("data overview")
+            st.dataframe(data.head())
+
+            st.subheader("Basic Information of the data")
+            st.write("shape of the data", data.shape)
+            st.write("columns in my data", data.columns)
+            st.write("missing value", data.isnull().sum())
+
+            st.subheader("i will show you stats of the data")
+            st.write(data.describe())
+        except Exception as e :
+            print("it is not working ",e)
+    else :
+        pass
+
+
+
+if __name__ == "__main__":
+    main()
+
+    
